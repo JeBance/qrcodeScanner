52a05f26a97dac11af7ab342c02f429c2919b538
Oleg Prudkov <oleg.prudkov@gmail.com>
2025-01-25 16:55:20 +0900

    Update app.js
    Added Method #2

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* Method #1 */
-
+/*
 function onScanSuccess(decodedText, decodedResult) {
 	try {
 		console.log(decodedResult);
@@ -19,3 +19,32 @@ function onScanFailure(error) {
 
 let qrScanner = new Html5QrcodeScanner("qrReader", { fps: 24, qrbox: {width: 350, height: 350} }, false);
 qrScanner.render(onScanSuccess, onScanFailure);
+*/
+
+/* Method #2 */
+
+const config = {
+	qrScan: {
+		fps: 10,
+		qrbox: {
+			width: 250,
+			height: 250
+		},
+		supportedScanTypes: [ Html5QrcodeScanType.SCAN_TYPE_CAMERA ]
+	}
+};
+
+const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+	try {
+		console.log(decodedResult);
+		let p = document.createElement('p');
+		p.textContent = 'Code matched = ' + decodedText;
+		qrReaderResult.append(p);
+		qrScanner.stop();
+	} catch(e) {
+		console.log(e);
+	}
+};
+
+let qrScanner = new Html5Qrcode("reader");
+qrScanner.start({ facingMode: "environment" }, config.qrScan, qrCodeSuccessCallback);
