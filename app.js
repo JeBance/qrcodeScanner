dc07935c15a83752902fa1871d430c6f654f05c5
Олег <JeBance>
2025-01-27 17:40:12 +0300

    fixed function printContent

--- app.js
+++ app.js
@@ -78,7 +78,7 @@ const printContent = (decodedText, decodedResult) => {
 		qrReaderResult.animation();
 		
 		if (isURLstring(decodedText)) {
-			browse.href = `${decodedText}`;
+			browse.setAttribute('href', decodedText);
 			browse.className = '';
 		} else {
 			browse.className = 'hide';
